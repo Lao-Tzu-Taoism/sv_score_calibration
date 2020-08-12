a59c0a78e136bdff5e87484fbac27319c046acee
Hui Zhang <zhtclz@foxmail.com>
2020-08-12 21:49:52 +0800

    add metric script

---
+++ bin/metric.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#  ./bin/metric.sh  veri_test.txt fu.score
+
+trials=$1
+calib_file=$2
+
+python ./VoxSRC2020/compute_min_dcf.py  --p-target 0.05 --c-miss 1 --c-fa 1 $calib_file  $trials
+python ./VoxSRC2020/compute_EER.py  --ground_truth $trials --prediction  $calib_file
+python voices_scorer/score_voices --label-column 1 --p-target 0.05 $calib_file $trials
