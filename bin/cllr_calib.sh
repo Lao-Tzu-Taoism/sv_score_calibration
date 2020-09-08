8274a186f2df1c8671e317757c35e6d22581f4ae
Hui Zhang <zhtclz@foxmail.com>
2020-09-08 11:13:54 +0800

    cllr calib

--- bin/cllr_calib.sh
+++ bin/cllr_calib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# ./bin/calib.sh --trails veri_test.txt --score_files "VoxCeleb1-O_eer1.7497_aamsoftmax_test.score VoxCeleb1-O_eer1.7922_amsoftmax_test.score"
+# ./bin/cllr_calib.sh --trails veri_test.txt --score_files "VoxCeleb1-O_eer1.7497_aamsoftmax_test.score VoxCeleb1-O_eer1.7922_amsoftmax_test.score"
 
 set -e
 set -u
@@ -12,6 +12,7 @@ log() {
     echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
 }
 
+root=cllr_calib
 tag="defualt"
 model=model.pt # model ckpt
 trails=trails  # SV trails
@@ -21,14 +22,14 @@ calib_file=score_${tag}.calib # calibration scores
 log "$0 $*"
 . ./bin/parse_options.sh
 
-python calibrate_scores.py \
+python $root/calibrate_scores.py \
   --label-column 1 \
   --log-llr \
   --save-model $model \
   $trails \
   $score_files
 
-python apply_calibration.py  \
+python $root/apply_calibration.py  \
   --label-column 1 \
   --log-llr  \
   $model  \
