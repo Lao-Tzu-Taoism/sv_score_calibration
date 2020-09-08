9667fd1e45516ebe0f9830719bb73313a95b3847
Hui Zhang <zhtclz@foxmail.com>
2020-09-08 11:11:56 +0800

    move metrics into tools dir

--- bin/metric.sh
+++ bin/metric.sh
@@ -5,6 +5,6 @@
 trials=$1
 calib_file=$2
 
-python ./VoxSRC2020/compute_min_dcf.py  --p-target 0.05 --c-miss 1 --c-fa 1 $calib_file  $trials
-python ./VoxSRC2020/compute_EER.py  --ground_truth $trials --prediction  $calib_file
-python voices_scorer/score_voices --label-column 1 --p-target 0.05 $calib_file $trials
+python tools/VoxSRC2020/compute_min_dcf.py  --p-target 0.05 --c-miss 1 --c-fa 1 $calib_file  $trials
+python tools/VoxSRC2020/compute_EER.py  --ground_truth $trials --prediction  $calib_file
+python tools/voices_scorer/score_voices --label-column 1 --p-target 0.05 $calib_file $trials
